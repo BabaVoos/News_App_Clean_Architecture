9879d5cf6d0d8fb2a934677c74eb65d4536d4364
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-08-18 06:38:27 +0300

    - Added Constants file - Added News API Service - Refactored Artcle Repository to adapt to the changes

--- lib/featuers/daily_news/data/repository/article_repository_impl.dart
+++ lib/featuers/daily_news/data/repository/article_repository_impl.dart
@@ -1,11 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/core/constants/constants.dart';
+import 'package:news_app_clean_architecture/featuers/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app_clean_architecture/featuers/daily_news/data/models/article.dart';
 import '../../../../core/resources/data_state.dart';
 import '../../domain/repository/article_repository.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: country,
+        category: category,
+      );
+
+      if (httpResponse.response.statusCode == 200) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataError(
+          DioException(
+            requestOptions: httpResponse.response.requestOptions,
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataError(e);
+    }
   }
 }
